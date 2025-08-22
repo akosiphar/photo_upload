d30c1dfcd1695b395f4e8ecc8d048d218417d6f6
akosiphar <raphgalag@gmail.com>
2025-08-22 22:59:55 +0800

    changed appscript link

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 // api/proxy.js
 export default async function handler(req, res) {
   const GOOGLE_APPS_SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycbw4IsFU8TfzoziATBz2mSwBU7ZW9Huk1KYxY3TwsvGxAK8oep_Glpz0B4_goTbrYSk97Q/exec";
+    "https://script.google.com/macros/s/AKfycbwDX5ec7zDfyimKgZwgCI42o4dGPjL6no5nOEleeg4hU2x7hgwb3EGxHBjP06ARC98a5Q/exec";
 
   try {
     const response = await fetch(GOOGLE_APPS_SCRIPT_URL, {
